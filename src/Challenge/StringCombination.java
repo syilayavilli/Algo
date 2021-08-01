8346eaee5c08e6892a6a4f118f8a7bad6acfb5e5
Seshadri Yilayavilli <seshadri.yilayavilli@fujitsu.com>
2021-08-01 10:04:57 -0700

    Initial

---
+++ src/Challenge/StringCombination.java
@@ -0,0 +1,62 @@
+package Challenge;
+
+import java.util.*;
+
+public class StringCombination {
+
+    public static void main(String[] args) {
+
+
+        // All possible strings
+        String str = "apple";
+
+        // n^2 ; but because of substring and print it will be n^3
+        for (int start=0; start < str.length(); start++) {
+            for (int end= start +1 ; end < str.length() + 1; end++) {
+                //we need length + 1 because the substring end is exclusive
+                String subStr = str.substring(start, end);
+                System.out.print(subStr + "|");
+
+            }
+        }
+
+        // we need to get "apple"
+        /*
+            apple
+            a p p le
+            a pp l e
+            a ppl e
+            a pple
+            ap p l e
+            app l e
+
+         */
+
+        printSubStringSets(str);
+
+    }
+
+    static void printSubStringSets(String str) {
+        List<String> resultList = new ArrayList();
+        printSubStringSets(str, 0, resultList);
+    }
+
+    static void printSubStringSets(String str, int start, List<String> resultList) {
+
+       if (start == str.length()) {
+           //store the result in a list
+           System.out.println("Result " + resultList);
+           return;
+        }
+
+       for (int end = start+1; end < str.length()+1; end++) {
+           String subStr = str.substring(start, end);
+           resultList.add(subStr);
+           printSubStringSets(str, end, resultList);
+
+           //backtrack  - Remove last - try without the below
+           resultList.remove(resultList.size() -1);
+        }
+
+    }
+}
