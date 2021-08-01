1474fbd43028db6fcc4a94b19b909ef2cf766b77
Seshadri Yilayavilli <seshadri.yilayavilli@fujitsu.com>
2021-08-01 08:50:41 -0700

    Initial Version

---
+++ src/Challenge/PermutationArray.java
@@ -0,0 +1,43 @@
+package Challenge;
+
+//Permutation - number of cases for an array will be n! i.e. 3! = 6 say for [1,2,3], [2,1,3],
+// [5,6,7]
+// Backtracking kind of problem
+
+import java.util.Arrays;
+
+public class PermutationArray {
+
+    public static void main(String[] args) {
+        int[] ary = {5,6,7};
+        perm(ary);
+    }
+
+    static void perm(int[] ary) {
+        perm(ary, 0);
+    }
+
+    static void perm(int[] ary, int index) {
+
+        if (index == ary.length) {
+            System.out.println(Arrays.toString(ary));
+            return;
+        }
+
+        for (int j = index; j < ary.length; j++) {
+            swap(ary, index, j);
+            perm(ary, index+1);
+
+            //backtracking
+            swap(ary, index, j);
+        }
+
+
+    }
+
+    public static void swap (int[] ary, int x, int y) {
+        int temp = ary[x];
+        ary[x] = ary[y];
+        ary[y] = temp;
+    }
+}
