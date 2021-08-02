89ade9c18adc9003491ea09b06177dfb6bb87649
Seshadri Yilayavilli <seshadri.yilayavilli@fujitsu.com>
2021-08-02 16:39:07 -0700

    optimized solution

--- src/Challenge/WildCardBinary.java
+++ src/Challenge/WildCardBinary.java
@@ -13,19 +13,20 @@ import java.util.Arrays;
 public class WildCardBinary {
 
     public static void main(String [] args) {
-        String str = "10?";
-        print(str.toCharArray(), 0);
+//        String str = "10?";
+//        print(str.toCharArray(), 0);
+//
+//        String str1 = "1?0?";
+//        print(str1.toCharArray(), 0);
+//
+//        String str2 = "????";
+//        print(str2.toCharArray(), 0);
+//
+//        String str3 = "1111";
+//        print(str3.toCharArray(), 0);
 
-        String str1 = "1?0?";
-        print(str1.toCharArray(), 0);
-
-        String str2 = "????";
-        print(str2.toCharArray(), 0);
-
-        String str3 = "1111";
-        print(str3.toCharArray(), 0);
-
-
+          String str4 = "1?0?";
+           System.out.println(printWildCard(str4));
     }
 
     public static void print(char[] str, int index) {
@@ -54,5 +55,23 @@ public class WildCardBinary {
 
         } else
             print(str, index+1);
+    }
+
+    /**
+     * Optimized Recursive solution to printWildCard for a given string.
+     * @param str
+     * @return
+     */
+    public static String printWildCard(String str) {
+        System.out.println(str);
+        // base scenario - print if no wild card is found in the string.
+        if (str.indexOf("?") == -1) {
+            return str;
+        } else { // call recursively for every wild card with 0, 1
+            int idx = str.indexOf("?");
+            return printWildCard(str.substring(0, idx)+"0"+str.substring(idx+1, str.length()))
+                    + " " +
+                    printWildCard(str.substring(0, idx)+"1"+str.substring(idx+1, str.length()));
+        }
     }
 }
