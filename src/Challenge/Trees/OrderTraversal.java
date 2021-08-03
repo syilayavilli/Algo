badd105c07ece61dd46a68ecdbbe3dc4050e7eef
Seshadri Yilayavilli <seshadri.yilayavilli@fujitsu.com>
2021-08-02 17:58:32 -0700

    initial

---
+++ src/Challenge/Trees/OrderTraversal.java
@@ -0,0 +1,41 @@
+package Challenge.Trees;
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class OrderTraversal {
+
+    public static void main(String[] args ) {
+
+        Node  root = new Node(0);
+        Node  node1 = new Node(1);
+        Node  node2 = new Node(2);
+        Node  node3 = new Node(3);
+        Node  node4 = new Node(4);
+
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node2.right = node4;
+
+        printInOrder(root);
+    }
+
+    static void printInOrder(Node node) {
+        //base condition
+        if (node == null) {
+            return;
+        }
+
+        printInOrder(node.left);
+        System.out.print(node.val + ",");
+        printInOrder(node.right);
+    }
+}
