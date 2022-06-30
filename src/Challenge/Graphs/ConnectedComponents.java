6584e37a25bf2f149e43fe76454992d05c40f8c6
Seshadri <ses_k@hotmail.com>
2022-06-29 18:34:18 -0700

    updated

--- src/Challenge/Graphs/ConnectedComponents.java
+++ src/Challenge/Graphs/ConnectedComponents.java
@@ -37,5 +37,115 @@ We also need to use isVisited
         dfs(--)
      }
  */
+
+import java.util.LinkedList;
+
+//print number of connected components
 public class ConnectedComponents {
+
+    static class UndirectedGraph {
+        int V;
+
+        LinkedList<Integer>[] adjList;
+
+        public UndirectedGraph(int vertices) {
+            V = vertices;
+            adjList = new LinkedList[V];
+        }
+
+        public void addEdge(int vertex, int c) {
+            LinkedList<Integer> list = adjList[vertex];
+            if (list == null) {
+                list = new LinkedList<Integer>();
+            }
+            list.add(c);
+            adjList[vertex] = list;
+        }
+
+        public void printGraphUsingDFS() {
+            boolean[] isVisited = new boolean[adjList.length];
+
+            for (int i = 0; i < adjList.length; i++ ) {
+                if(!isVisited[i] ) {
+                    dfs(i, isVisited);
+                }
+            }
+        }
+
+        public void printNumberOfConnectedComponents(){
+            boolean[] isVisited = new boolean[adjList.length];
+            int count = 0;
+
+            for (int i=0; i < adjList.length; i++) {
+                if(!isVisited[i]) {
+                    dfs(i, isVisited);
+                    count++; //count number of connected components
+                }
+            }
+            System.out.println("Number of connected components are " + count);
+        }
+
+        public void dfs(int i, boolean[] isVisited) { //36:05
+
+            isVisited[i] = true;
+            System.out.print(i + " ");
+
+            LinkedList<Integer> list = adjList[i];
+            if (list != null) {
+                for (Integer v : list) {
+                    if (v != null && !isVisited[v])
+                        dfs(v, isVisited);
+                }
+            }
+        }
+
+        public boolean isCycle() {
+            boolean[] isVisited = new boolean[adjList.length];
+            boolean[] isAlive = new boolean[adjList.length];
+
+            for(int i = 0; i < adjList.length; i++) {
+                if (!isVisited[i]) {
+                    if (isDFSCycle(i, isVisited, isAlive)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public boolean isDFSCycle(Integer v, boolean[] isVisited, boolean[] isAlive) {
+            isVisited[v]= true;
+            isAlive[v]= true;
+
+            LinkedList<Integer> list = adjList[v]; //get neighbors of v or all vertices connected to v
+            if (list != null) {
+                for (Integer c : list) {
+                    if (c != null && !isVisited[c]) { // not visited
+                        if(isDFSCycle(c, isVisited, isAlive)) {
+                            return true;
+                        }
+                    } if (c != null && isVisited[c] && isAlive[c]) { // is is Visited & Alive also which means there is a cycle
+                        return true;  //if cycle just simply return
+                    }
+                }
+            }
+            isAlive[v] = false; //backtrack because of the path
+
+            return false; //no cycle found so return false.
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        UndirectedGraph graph = new UndirectedGraph(5);
+        graph.addEdge(0, 3);
+        graph.addEdge(0, 4);
+        graph.addEdge(1, 2);
+
+        //graph.printGraphUsingBFS(graph);
+
+        graph.printNumberOfConnectedComponents();
+    }
 }
--- src/Challenge/Graphs/CountIslandBinaryMatrix.java
+++ src/Challenge/Graphs/CountIslandBinaryMatrix.java
@@ -1,12 +1,17 @@
 package Challenge.Graphs;
+
+import java.util.LinkedList;
+
 //Number of Islands
-//Binary matrix  1- land 0 - water. Find number of islands from this matrix
-/*
-    0  1 0  1  1
-    1  0 0  1  1
-    0  0 1  1  1
-    0  0 0  0  0
-    1  1 1  0  1
+//Binary matrix  1 represents land 0 represents water.
+// Find number of islands from this matrix
+/* This is an undirected graph
+
+    0  1  0  1  1
+    1  0  0  1  1
+    0  0  1  1  1
+    0  0  0  0  0
+    1  1  1  0  1
 
     there are 4 islands
 
@@ -28,17 +33,15 @@ package Challenge.Graphs;
        for (int c = 0; c< ...)
            matric[r][c]
 
-
-     boolean [][] isVisited[matrix.length][matrix[0].length];
-
+    boolean [][] isVisited[matrix.length][matrix[0].length];
 
     matrix[1][2]
 
-    if we dont want to hold isVisited due to additional space,
+    if we don't want to hold isVisited due to additional space,
     we can change the inout of 1 is visited to -1 and we can replace back -1 to 1
 
     3*3 matrix - leaving the node will be 8 directions
-    8 statements to travese the direction or an easier way small trick
+    8 statements to traverse the direction or an easier way
     small trick is
        either use the above direction trick  or call dfs multiple times i.e. 8 times
        dfs(matrix, row+1, col); //up - add boundary condition
@@ -47,12 +50,85 @@ package Challenge.Graphs;
         .... 8 statements
 
         static int[[][] directions = {
-            {-1, 0},
-            {},
-
+            {-1, 0}, //up
+            {1,0}, //down
+            {0, -1}, //left
+            {0, 1}, //right
+            {-1, -1}, //up left
+            {1,1}, //down right
+            {1,-1}, //down left
+            {1,1} //down right
         };
 
-    //Undirectedgrpahdemo and count islands
+    //Undirected graph demo and count islands
  */
 public class CountIslandBinaryMatrix {
+
+    //pair fo row and column
+    //first element is row, second element is column
+    static int[][] directions = {
+        {-1, 0}, //up
+        {1,0}, //down
+        {0, -1}, //left
+        {0, 1}, //right
+        {-1, -1}, //up left
+        {1,1}, //down right
+        {1,-1}, //down left
+        {-1,1} //down right
+    };
+
+    public static void main(String args[]) {
+
+        int[][] matrix = {
+                {1,1,0,0,0},
+                {0,1,0,0,1},
+                {1,0,0,1,1},
+                {0,0,0,0,0},
+                {1,0,1,0,1}
+        };
+        System.out.println(" Number of Islands is " + countIslands(matrix));
+    }
+
+    public static int countIslands(int[][] matrix) {
+        int count = 0;
+
+        //We need to traverse all of the matrix - so we will use double for loop
+        for (int row = 0; row < matrix.length; row++) { //for each node/vertex in the graph
+            for (int col = 0; col < matrix[0].length; col++) {
+                //If the cell is 1 and not visited traverse
+                if (matrix[row][col] == 1) { //1 also means a vertex in this problem
+                    dfs(matrix, row, col);
+                    count++; //count number of connected components
+                    //note here we are not using isVisited just to save some space, Check how space is saved
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(int[][] matrix, int row, int col) {
+        //work as visited by marking 1 to -1
+        matrix[row][col] = -1;
+
+        // every cell needs to check its neighbors deep using dfs
+        //and there are eight neighbors for each cell. So traverse all
+        for (int dir= 0; dir<directions.length; dir++) {
+
+            int newRow = row + directions[dir][0]; //0 cell is row and 1 cell is col
+            int newCol = col + directions[dir][1];
+
+            if (isValid(matrix, newRow, newCol) && (matrix[newRow][newCol] == 1)){
+                //do dfs
+                dfs(matrix, newRow, newCol);
+            }
+        }
+
+    }
+
+    public static boolean isValid(int[][] matrix, int r, int c) {
+        if (r < 0  || r >= matrix.length || c < 0 || c >= matrix[0].length) {
+            return false;
+        }
+        return true;
+    }
 }
